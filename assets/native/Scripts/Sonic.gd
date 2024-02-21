9a9b6df385810cb3eba4f3a79c67b14703867a00
dotdotmod <tybaxter2000@gmail.com>
2024-02-21 09:31:44 +0200

    Better test stage

--- assets/native/Scripts/Sonic.gd
+++ assets/native/Scripts/Sonic.gd
@@ -1,8 +1,16 @@
 extends KinematicBody
 
+var player_id := "_p1"
 
-var run_speed
-var max_run_speed
+var run_speed : float
+var max_run_speed : float
 
-var active_speed
-var max_active_speed
+var active_speed : float
+var max_active_speed : float
+
+export var default_down_vec : Vector3
+
+func _physics_process(delta):
+
+	var raw_input_direction = Vector3( Input.get_action_strength("move_backward" + player_id), 0, Input.get_action_strength("move_backwards" + player_id) - Input.get_action_strength("move_forward" + player_id) )
+	var rotated_input_dir = raw_input_direction.rotated()
